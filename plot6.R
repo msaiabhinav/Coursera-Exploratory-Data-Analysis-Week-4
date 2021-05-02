675de6703eeced3084d79871191ba854cdbfdfdb
msaiabhinav <72513057+msaiabhinav@users.noreply.github.com>
2021-05-02 20:16:55 +0530

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,8 +1,10 @@
-## read in emissions data and classification code
-emissions_data <- readRDS("summarySCC_PM25.rds")
+## Starting the Program
+                                  
+## reading the emissions data and classification code
+emissions_data <-  readRDS ( "summarySCC_PM25.rds" )
 class_code <- readRDS("Source_Classification_Code.rds")
 
-## subset data from Baltimore City, LA county and from type "on road"
+                                                          ## Subset Data from Baltimore City, LA county and from type "ON ROAD"
 baltLA_car_data <- subset(emissions_data, emissions_data$fips=="24510" |
                                   emissions_data$fips=="06037" &
                                   emissions_data$type=="ON-ROAD")
@@ -11,11 +13,15 @@ baltLA_car_year <- aggregate(baltLA_car_data$Emissions,
                              FUN=sum)
 colnames(baltLA_car_year) <- c("City", "Year", "Emissions")
 
-## create plot comparing emissions from motor vehicles in Baltimore and LA from 1999-2008
-library(ggplot2)
-png(filename = "plot6.png")
-qplot(Year, Emissions, data = baltLA_car_year, color = City, geom = "line") +
-        ggtitle("Emissions of PM2.5 in Baltimore City (24510) and LA County (06037)") + 
-        ylab("Total Emissions from motor vehicles (tons)") + 
-        xlab("Year") 
+                                                ## creating plot which compares emissions from motor vehicles in Baltimore and LA between 1999 and 2008
+## with the help of ggplot2
+library ( ggplot2 )
+png( filename = "plot6.png" ) ## title
+## labelling and plotting
+qplot( Year, Emissions, data = baltLA_car_year, color = City, geom = "line" )  +
+        ggtitle( "Emissions of PM2.5 in Baltimore City (24510) and LA County (06037)")  + 
+        ylab( "Total Emissions from motor vehicles (tons)")  + 
+        xlab( "Year" ) 
 dev.off()
+
+## End of Program
